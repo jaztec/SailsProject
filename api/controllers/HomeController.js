84a5caa45690c5abb4e7eb985100178a1649eff4
Jasper van Herpt <jasper@jaspervanherpt.com>
2015-07-11 23:23:19 +0200

    Switched to jade rendering engine and refactored default way of entering the homepage. Moved the view pages to a more structured folder layout

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,16 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function (req, res) {
+
+    res.status(200);
+
+    res.view('pages/home', {});
+  }
+};
+
